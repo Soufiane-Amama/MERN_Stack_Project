548eabc9f82418093d00b7a820664fc5376a7345
Soufiane Amama <soufianeamama70@gmail.com>
2023-12-08 23:10:56 +0100

    Fetching Data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const jsonData = await response.json()
+
+      if (response.ok) {
+        setWorkouts(jsonData)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
   return (
-    <div className="home">Home</div>
+    <div className="home">
+    <div className="workouts">
+      {workouts && workouts.map(workout => (
+        <WorkoutDetails workout={workout} key={workout._id} />
+      ))}
+    </div>
+  </div>
   )
 }
 
