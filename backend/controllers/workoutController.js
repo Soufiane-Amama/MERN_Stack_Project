62259b9e9b1f1dc265e413596213fb170b89885c
Soufiane Amama <soufianeamama70@gmail.com>
2023-12-08 20:21:29 +0100

    Controllers (part 2)

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -8,6 +8,7 @@ const getWorkouts = async (req, res) => {
   res.status(200).json(workouts)
 }
 
+
 // get a single workout
 const getWorkout = async (req, res) => {
   const { id } = req.params
@@ -25,6 +26,7 @@ const getWorkout = async (req, res) => {
   res.status(200).json(workout)
 }
 
+
 // create a new workout
 const createWorkout = async (req, res) => {
   const {title, load, reps} = req.body
@@ -38,15 +40,42 @@ const createWorkout = async (req, res) => {
   }
 }
 
+
 // delete a workout
 const deleteWorkout = async (req, res) => {
+    const { id } = req.params
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({error: 'No such workout'})
+    }
+  
+    const workout = await Workout.findOneAndDelete({_id: id})
+  
+    if(!workout) {
+      return res.status(400).json({error: 'No such workout'})
+    }
+  
+    res.status(200).json(workout)
+  }
 
-}
 
 // update a workout
-const updateWorkout = async (req, res) => {
+  const updateWorkout = async (req, res) => {
+    const { id } = req.params
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({error: 'No such workout'})
+    }
+  
+    const workout = await Workout.findOneAndUpdate({_id: id}, { ...req.body }) 
+  
+    if (!workout) {
+      return res.status(400).json({error: 'No such workout'})
+    }
+  
+    res.status(200).json(workout)
+  }
 
-}
 
 module.exports = {
   getWorkouts,
