baf217abf3bf423fbc88443ff44da6593d143db2
Soufiane Amama <soufianeamama70@gmail.com>
2023-12-10 02:48:02 +0100

    Finishing Touches - تعديل شكل التاريخ

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,5 +1,7 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
+// date fns ==> Link library: https://date-fns.org/ ==> install: npm i date-fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext()
@@ -20,8 +22,8 @@ const WorkoutDetails = ({ workout }) => {
         <h4>{workout.title}</h4>
         <p><strong>Load (kg): </strong>{workout.load}</p>
         <p><strong>Number of reps: </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-        <span onClick={handleClick}>delete</span>
+        <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p> {/* تعديل طريقة اظهار التاريخ بواسطة مكتبة date fns */}
+        <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
       </div>
     )
   }
